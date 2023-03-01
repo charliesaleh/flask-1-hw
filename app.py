6f8592916acffb7fc8fe723c84f641f983273bc1
Charlie Saleh <charliesaleh@outlook.com>
2023-03-01 06:31:50 -0500

    sql hw

--- app.py
+++ app.py
@@ -17,20 +17,18 @@ def pokeapi():
         url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}'
         response = requests.get(url)
         if response.ok:
-            pokemon_data = response.json()
-            pokemon_list = []
-            for pokemon in pokemon_data:
-                get_pokemon_info = {
-                    "Name": pokemon["name"],
-                    "Abilities": pokemon["abilities"][0]['ability']['name'],
-                    "BaseExperience": pokemon["base_experience"],
-                    "FrontShinyURL": pokemon["sprites"]["front_shiny"],
-                    "AttackBaseStat": pokemon["stats"][0]["base_stat"],
-                    "HPBaseStat": pokemon["stats"][0]["base_stat"],
-                    "DefenseBaseStat": pokemon["stats"][0]["base_stat"],
+            pokemon = response.json()
+            get_pokemon_info = {
+                "Name": pokemon["name"],
+                "Abilities": pokemon["abilities"][0]['ability']['name'],
+                "BaseExperience": pokemon["base_experience"],
+                "FrontShinyURL": pokemon["sprites"]["front_shiny"],
+                "AttackBaseStat": pokemon["stats"][0]["base_stat"],
+                "HPBaseStat": pokemon["stats"][0]["base_stat"],
+                "DefenseBaseStat": pokemon["stats"][0]["base_stat"],
                 }
-                pokemon_list.append(get_pokemon_info)
-            return render_template('pokeapi.html', pokemon_list=pokemon_list)
+            print(get_pokemon_info)
+            return render_template('pokeapi.html', get_pokemon_info=get_pokemon_info)
         else:
             "This pokemon does not exist"
     return render_template('pokeapi.html')
